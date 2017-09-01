17e8bf3a0310c0610f1f664af776354ca983111f
Srinivasulu3264 <srinivasulu.budharapu@vmokshagroup.com>
2017-09-01 11:29:42 +0530

    Initial Commit

---
+++ GitXcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitXcode
+//
+//  Created by Vmoksha on 01/09/17.
+//  Copyright © 2017 srinivas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitXcode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitXcode
+//
+//  Created by Vmoksha on 01/09/17.
+//  Copyright © 2017 srinivas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
