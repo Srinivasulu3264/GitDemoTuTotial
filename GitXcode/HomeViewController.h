4c14435ac0c6804f4f7e81fe4478836405e805d0
Srinivasulu3264 <srinivasulu.budharapu@vmokshagroup.com>
2017-09-01 11:44:43 +0530

    homeVc added

---
+++ GitXcode/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  GitXcode
+//
+//  Created by Vmoksha on 01/09/17.
+//  Copyright © 2017 srinivas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ GitXcode/HomeViewController.m
@@ -0,0 +1,41 @@
+
+//
+//  HomeViewController.m
+//  GitXcode
+//
+//  Created by Vmoksha on 01/09/17.
+//  Copyright © 2017 srinivas. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"hello HomeVC");
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
