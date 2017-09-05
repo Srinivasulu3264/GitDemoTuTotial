adfd384906cde93fcca998296f996996fc62e991
Srinivas3264 <srinivas.ios2017@gmail.com>
2017-09-05 11:41:05 +0530

    added EmpViewController

---
+++ GitXcode/EmployeeViewController.h
@@ -0,0 +1,13 @@
+//
+//  EmployeeViewController.h
+//  GitXcode
+//
+//  Created by Vmoksha on 05/09/17.
+//  Copyright © 2017 srinivas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmployeeViewController : UIViewController
+
+@end
---
+++ GitXcode/EmployeeViewController.m
@@ -0,0 +1,37 @@
+//
+//  EmployeeViewController.m
+//  GitXcode
+//
+//  Created by Vmoksha on 05/09/17.
+//  Copyright © 2017 srinivas. All rights reserved.
+//
+
+#import "EmployeeViewController.h"
+
+@interface EmployeeViewController ()
+
+@end
+
+@implementation EmployeeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
